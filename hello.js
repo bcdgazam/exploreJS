41e9383454e32cdf4c16428f2fcb51cba773f65f
bcdazam <bcdazam@gmail.com>
2024-02-20 10:30:52 +0600

    String Number Boolean

--- hello.js
+++ hello.js
@@ -1,5 +1,25 @@
 // console.log(20)
 // console.log('Good Morning')
 
+// variable
+//number
 var bananaPrice = 20;
 console.log(bananaPrice);
+
+var temperature = 90;
+console.log (temperature);
+
+var potatoPrice = 25;
+console.log(potatoPrice);
+
+var seenAfter = 21;
+console.log (typeof seenAfter);
+
+//string
+var name = "Modon kumar";
+console.log (name);
+
+//boolean
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
