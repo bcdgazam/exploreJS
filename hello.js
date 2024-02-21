a1669ab6ea6bedf5287b6fce50a4b8d3f2bdaeaf
bcdazam <bcdazam@gmail.com>
2024-02-21 11:36:56 +0600

    numbers

--- hello.js
+++ hello.js
@@ -24,9 +24,24 @@ var isHot = true;
 var isRich = false;
 console.log(typeof isHot);
 
-//lowercase
+//Course Status – Programming Hero_6 (string)
 var promise = 'I will be a Professional Programmer IN The future';
 console.log(promise.toLowerCase());
 console.log(promise.toUpperCase());
 console.log(promise.indexOf('a'))
 console.log(promise.split('Programmer'))
+
+//Course Status – Programming Hero_7 (number)
+var number1 = 24;
+var number2 = '10.5';
+// number1 = ''+number1; //number to string
+number2 = +number2; //string to number
+// number2 = parseInt(number2);
+// number2 = parseFloat(number2);
+console.log(number1 + number2);
+
+var number1 = 0.1;
+var number2 = 0.2;
+var total = number1 + number2;
+total = total.toFixed(1); //decimal precision
+console.log(total);
