1f3e7bb5b4527325c1dca1f829ae2d7789b441b6
bcdazam <bcdazam@gmail.com>
2024-02-29 12:16:47 +0600

    Operators done

--- 12_8.js
+++ 12_8.js
@@ -9,10 +9,27 @@
 --	Decrement */
 
 
-//Addition
-
-var number1 = 25;
-var number2 = 35;
+var number1 = 21;
+var number2 = 10;
 
 var total = number1 + number2;
 console.log(total);
+
+var total2 = number1 - number2;
+console.log(total2);
+
+var total3 = number1 * number2;
+console.log(total3);
+
+var total4 = number1 / number2;
+console.log(total4);
+
+var total5 = number1 % number2;
+console.log(total5);
+
+number1 ++;
+console.log(number1);
+
+number2 --;
+console.log(number2);
+
